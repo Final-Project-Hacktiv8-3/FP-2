dcfd112cd5b9a671288b383f12653c80873df903
Eureka <caesarioarya288@gmail.com>
2023-10-31 15:56:27 +0700

    adding redux

---
+++ src/redux/adding/action.js
@@ -0,0 +1,56 @@
+export const addToCart = (title,image,id,price,stuff) => {
+  console.log(stuff)
+  const userId = localStorage.getItem('userId');
+  return {
+    type: 'ADD_TO_CART',
+    payload: { 
+      id: id,
+      usersId:userId,
+      image: image,
+      price: price,
+      title: title,
+      quantity: stuff ,
+      restQuantity: 20-stuff || 19,
+     },
+  };
+};
+
+export const plusQuantity = (title,image,id,price,stuff,restQuantity) => {
+  return {
+    type: 'PLUS_TO_CART',
+    payload: { 
+      id: id,
+      image: image,
+      price: price,
+      title: title,
+      quantity: stuff,
+      restQuantity: restQuantity,
+     },
+  };
+};
+
+export const minQuantity = (title,image,id,price,stuff,restQuantity) => {
+  return {
+    type: 'MIN_TO_CART',
+    payload: { 
+      id: id,
+      image: image,
+      price: price,
+      title: title,
+      quantity: stuff,
+      restQuantity: restQuantity,
+     },
+  };
+};
+
+export const deleteCart = (id) => {
+  return {
+    type: 'DELETE_CART',
+    payload: { 
+      id: id,
+     },
+  };
+};
+
+
+
---
+++ src/redux/products/constant.js
@@ -0,0 +1,4 @@
+export const START_FETCHING_PRODUCTS = 'redux/products/START_FETCHING_PRODUCTS';
+export const SUCCESS_FETCHING_PRODUCTS =
+  'redux/products/SUCCESS_FETCHING_PRODUCTS';
+export const ERROR_FETCHING_PRODUCTS = 'redux/products/ERROR_FETCHING_PRODUCTS';
