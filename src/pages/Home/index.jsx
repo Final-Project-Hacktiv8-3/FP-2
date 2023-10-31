341e6954efe0b6e3b2e9b6645c187b6c7c4d7bf5
shidqi29 <shidqiamirulhaq@gmail.com>
2023-10-31 15:29:42 +0700

    feat(navbar): add navbar component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,9 @@
-import { Button, DarkThemeToggle } from "flowbite-react";
+import { NavBar } from "@components";
 
 export const Home = () => {
   return (
-    <div>
-      <h1 className="text-primary bg-primary text-3xl">Home</h1>
-      <Button className="dark:text-red-500">Click me</Button>
-      <DarkThemeToggle />
-    </div>
+    <>
+      <NavBar />
+    </>
   );
 };
